b77275b785cff48ae6611e6131a868b3dec97617
João victor <joaov0044@gmail.com>
2020-07-16 11:05:21 -0300

    projeto-galeria

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Arquivos sass
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos js
+import './js/core/includes'
+import './js/plugins/cityButtons'
